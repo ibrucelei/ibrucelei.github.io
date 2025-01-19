05b96151d23ca916806a4e68746f783f0dfa70f6
gkwnight <sunyux2187@gmail.com>
2025-01-19 09:42:09 +0800

    added link of apk

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,28 +1,35 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { SiGithub, SiGooglechrome, SiBrave } from "react-icons/si";
+import { SiGithub, SiGooglechrome, SiBrave, SiAndroid } from "react-icons/si";
 
 function Toolstack() {
   let timestamp = '?v=' + new Date().getTime();
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <a href={"https://github.com/kasware-wallet/extension"+timestamp} style={{ color: "white" }} target="_blank" rel="noopener noreferrer">
-          <SiGithub />
+        <a href={"https://github.com/kasware-wallet/kasware-app/releases" + timestamp} style={{ color: "white" }} target="_blank" rel="noopener noreferrer">
+          <SiAndroid />
         </a>
-        <h3 className="purple">GitHub </h3>
+        <h3 className="purple">Android APK </h3>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <a href={"https://chromewebstore.google.com/detail/kasware-wallet/hklhheigdmpoolooomdihmhlpjjdbklf"+timestamp} style={{ color: "white" }} target="_blank" rel="noopener noreferrer">
+        <a href={"https://chromewebstore.google.com/detail/kasware-wallet/hklhheigdmpoolooomdihmhlpjjdbklf" + timestamp} style={{ color: "white" }} target="_blank" rel="noopener noreferrer">
           <SiGooglechrome />
         </a>
         <h3 className="purple">Chrome </h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <a href={"https://chromewebstore.google.com/detail/kasware-wallet/hklhheigdmpoolooomdihmhlpjjdbklf"+timestamp} style={{ color: "white" }} target="_blank" rel="noopener noreferrer">
+        <a href={"https://chromewebstore.google.com/detail/kasware-wallet/hklhheigdmpoolooomdihmhlpjjdbklf" + timestamp} style={{ color: "white" }} target="_blank" rel="noopener noreferrer">
           <SiBrave />
         </a>
         <h3 className="purple">Brave </h3>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <a href={"https://github.com/kasware-wallet/extension" + timestamp} style={{ color: "white" }} target="_blank" rel="noopener noreferrer">
+          <SiGithub />
+        </a>
+        <h3 className="purple">GitHub </h3>
       </Col>
     </Row>
   );
